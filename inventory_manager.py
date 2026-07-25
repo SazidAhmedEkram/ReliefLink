95b8a926fbd5fcdb99602be45c92b603cc8ec902
Borshon Debnath <borshon128@gmail.com>
2026-07-25 22:44:53 +0600

    Update inventory manager module

--- inventory_manager.py
+++ inventory_manager.py
@@ -4,7 +4,7 @@ from file_handler import load_inventory, save_inventory
 # Inventory Menu
 def inventoryMenu():
     while True:
-        print("=============================")
+        print("==============================")
         print("Inventory Management")
         print("=============================")
         print("1.Add Item")
